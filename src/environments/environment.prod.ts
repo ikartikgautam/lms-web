38556675b419220fbe6a89a3c8388a35040c4d8f
Kartik Gautam <gautam.kartik05@gmail.com>
2021-08-28 12:35:18 +0530

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyARt-xQrPmlzgSV19HfzrxouCR8p3DmwD8",
+    authDomain: "flipr-lms.firebaseapp.com",
+    projectId: "flipr-lms",
+    storageBucket: "flipr-lms.appspot.com",
+    messagingSenderId: "386650313649",
+    appId: "1:386650313649:web:6e5a2db5ac9a6184c46390",
+    measurementId: "G-1BCR77012Q"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyARt-xQrPmlzgSV19HfzrxouCR8p3DmwD8",
+    authDomain: "flipr-lms.firebaseapp.com",
+    projectId: "flipr-lms",
+    storageBucket: "flipr-lms.appspot.com",
+    messagingSenderId: "386650313649",
+    appId: "1:386650313649:web:6e5a2db5ac9a6184c46390",
+    measurementId: "G-1BCR77012Q"
+  }
 };
 
 /*
