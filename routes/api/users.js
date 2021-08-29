55c7aad20540cd85aa09a0d81d610f04c0715a7b
Mridul Jain <itsme.mriduljain@gmail.com>
2021-08-29 11:24:14 +0530

    changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
 const User = require('../../models/User')
 
 
 router.get('/',(req,res,next)=>{
-=======
-
-const User = require('../../models/User')
-
-
-// router.use(cors());
-// router.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-// });
-
-
-router.get('/', (req, res, next) => {
->>>>>>> 29e1e136d3c2b91329526fb6756969f89a105ba5
     res.send("Get Users")
 })
 
@@ -46,23 +28,17 @@ router.post('/', (req, res, next) => {
             })
             return user.save()
 
-                .then((user) => {
-                    return res.json(user)
+            .then((user) => {
+                req.user = user;
+                console.log("user created")
+                console.log(req.user.id)
+                return res.json(user)
                 })
 
-<<<<<<< HEAD
-        .then((user)=>{
-            req.user = user;
-            console.log(req.user.id)
-            return res.json(user)
-        })
-   
     })
         .catch((err)=>{
             console.log(err.message);
             return res.status(500).send("server error")
-=======
->>>>>>> 29e1e136d3c2b91329526fb6756969f89a105ba5
         })
 })
 
--- server.js
+++ server.js
@@ -2,26 +2,19 @@ const express = require('express');
 const connectDB = require('./config/db')
 const app = express();
 const path = require('path')
-const cors = require('cors');
 
 const usersRoute = require('./routes/api/users')
 
 
 connectDB();
 
-// app.use(cors({
-//     origin: ['http://localhost:5000/', 'http://localhost:4200/']
-// }));
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-});
-
 app.use(express.json({ extended: false }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/api/users', usersRoute);
 
