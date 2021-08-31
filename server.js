a9c9c009633558087155b39123915911618ec831
Mridul Jain <itsme.mriduljain@gmail.com>
2021-08-31 22:03:48 +0530

    changes reverted

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ app.use((req, res, next) => {
 app.use('/api/users', usersRoute);
 app.use('/api/createclass',classRoute)
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static('client/build'));
 
-  app.get('*',(req,res)=>{
-      res.sendFile(path.resolve(___dirname,'src','build','index.html'))
-  })
-}
+//   app.get('*',(req,res)=>{
+//       res.sendFile(path.resolve(___dirname,'src','build','index.html'))
+//   })
+// }
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`)
